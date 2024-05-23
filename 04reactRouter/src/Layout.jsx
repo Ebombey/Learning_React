f8e53fc80748398105600f0b2e1c36ffaf5c44c3
Naveen <naveenraikwar200@gmail.com>
2024-05-24 01:51:48 +0530

    know new way of route

--- 04reactRouter/src/Layout.jsx
+++ 04reactRouter/src/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
-function Route() {
+function Layout() {
   return (
     <>
       <Header />
@@ -12,4 +12,4 @@ function Route() {
   );
 }
 
-export default Route;
+export default Layout;
--- 04reactRouter/src/main.jsx
+++ 04reactRouter/src/main.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Route from "./Route.jsx";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
+import Layout from "./Layout.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Route />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+//First to display route
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Route />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+//second way to display route
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
